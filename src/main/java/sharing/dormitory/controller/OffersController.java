9c535c3efb9a21c4504f32fe57a23d9fc953a283
Ilia Nemankov <ilia.nemankov@gmail.com>
2021-01-06 12:37:56 +0300

    Add filter for offers

--- src/main/java/sharing/dormitory/controller/OffersController.java
+++ src/main/java/sharing/dormitory/controller/OffersController.java
@@ -4,11 +4,9 @@ import lombok.AllArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import sharing.dormitory.db.model.Offer;
+import sharing.dormitory.db.model.Service;
 import sharing.dormitory.service.OffersService;
 import sharing.dormitory.service.UserService;
 
@@ -22,11 +20,17 @@ public class OffersController {
     private final OffersService offersService;
 
     @GetMapping("/offers")
-    public String offers(Authentication authentication, Model model) {
+    public String offers(@RequestParam(name = "show_my", required = false) Boolean showMy, Authentication authentication, Model model) {
         Integer userId = userService.getUser(authentication.getName()).getId();
-        List<Offer> offers = offersService.getOffers(userId);
+        List<Offer> offers;
+        if (showMy != null && showMy) {
+            offers = offersService.getUserOffers(userId);
+        } else {
+            offers = offersService.getOffers(userId);
+        }
         model.addAttribute("offers", offers);
         model.addAttribute("newOffer", new Offer());
+        model.addAttribute("showMy", showMy);
         model.addAttribute("userName", authentication.getName());
         return "offers";
     }
@@ -34,13 +38,19 @@ public class OffersController {
     @PostMapping("/offers/delete/{id}")
     public String delete(Authentication authentication, Model model, @PathVariable Integer id) {
         offersService.deleteOffer(id);
-        return offers(authentication, model);
+        return offers(false, authentication, model);
+    }
+
+    @GetMapping("/offers/create")
+    public String createOfferPage(Model model) {
+        model.addAttribute("newOffer", new Offer());
+        return "create_offer";
     }
 
     @PostMapping("/offers/create")
     public String createOffers(@ModelAttribute Offer offer, Authentication authentication, Model model) {
         Integer userId = userService.getUser(authentication.getName()).getId();
         offersService.createOffer(offer, userId);
-        return offers(authentication, model);
+        return offers(false, authentication, model);
     }
 }
---
+++ src/main/resources/static/js/offer_validation.js
@@ -0,0 +1,15 @@
+function validateOfferForm(form) {
+	let name = form.name.value;
+	let description = form.description.value;
+
+    let valid = true;
+
+	if (!(isPresented(name, "Name", "name"))) {
+		valid = false;
+	} else if (name.length < 5) {
+        showWarning("Name must contain at least 5 symbols", "name");
+        valid = false;
+    }
+
+    return valid
+}
